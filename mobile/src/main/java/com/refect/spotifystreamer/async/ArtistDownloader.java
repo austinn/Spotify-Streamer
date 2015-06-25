f19514fa42f44b4a0c4d6f3c625b76dc3d298126
Austin Nelson <nelson.b.austin@gmail.com>
2015-06-25 09:12:45 -0400

    Added network check

--- mobile/src/main/java/com/refect/spotifystreamer/async/ArtistDownloader.java
+++ mobile/src/main/java/com/refect/spotifystreamer/async/ArtistDownloader.java
@@ -2,6 +2,7 @@ package com.refect.spotifystreamer.async;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.refect.spotifystreamer.adapters.ArtistAdapter;
@@ -14,6 +15,7 @@ import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.RetrofitError;
 
 /**
  * Created by anelson on 6/15/15.
@@ -35,23 +37,29 @@ public class ArtistDownloader extends AsyncTask<String, Void, List<ArtistModel>>
 
     @Override
     protected List<ArtistModel> doInBackground(String... params) {
-
-        SpotifyApi api = new SpotifyApi();
-        SpotifyService spotify = api.getService();
-        ArtistsPager results = spotify.searchArtists(params[0]);
         ArrayList<ArtistModel> models = new ArrayList<>();
-        for(Artist artist : results.artists.items) {
 
-            ArtistModel model = new ArtistModel();
-            model.setName(artist.name);
-            model.setId(artist.id);
-            if(artist.images.size() > 0) {
-                model.setUrl(artist.images.get(0).url);
+        try {
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
+            ArtistsPager results = spotify.searchArtists(params[0]);
+
+            for(Artist artist : results.artists.items) {
+
+                ArtistModel model = new ArtistModel();
+                model.setName(artist.name);
+                model.setId(artist.id);
+                if(artist.images.size() > 0) {
+                    model.setUrl(artist.images.get(0).url);
+                }
+                if(artist.genres.size() > 0) {
+                    model.setGenre(artist.genres.get(0));
+                }
+                models.add(model);
             }
-            if(artist.genres.size() > 0) {
-                model.setGenre(artist.genres.get(0));
-            }
-            models.add(model);
+        } catch(RetrofitError error) {
+            Log.e("Error: ", error.getMessage());
+            return null;
         }
 
         return models;
@@ -60,7 +68,9 @@ public class ArtistDownloader extends AsyncTask<String, Void, List<ArtistModel>>
     @Override
     protected void onPostExecute(List<ArtistModel> artists) {
 
-        if(artists.isEmpty()) {
+        if(artists == null) {
+            Toast.makeText(mContext, "Oh no! Something went wrong :( Make sure you have an internet connection", Toast.LENGTH_SHORT).show();
+        } else if(artists.isEmpty()) {
             Toast.makeText(mContext, "No artists match this search. Please redefine your search", Toast.LENGTH_SHORT).show();
         } else {
             artistAdapter.setModels(artists);
--- mobile/src/main/java/com/refect/spotifystreamer/async/TrackDownloader.java
+++ mobile/src/main/java/com/refect/spotifystreamer/async/TrackDownloader.java
@@ -3,6 +3,7 @@ package com.refect.spotifystreamer.async;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.refect.spotifystreamer.adapters.ArtistAdapter;
@@ -20,6 +21,7 @@ import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
 import kaaes.spotify.webapi.android.models.Track;
 import kaaes.spotify.webapi.android.models.Tracks;
+import retrofit.RetrofitError;
 
 /**
  * Created by anelson on 6/15/15.
@@ -41,30 +43,35 @@ public class TrackDownloader extends AsyncTask<String, Void, List<TrackModel>> {
 
     @Override
     protected List<TrackModel> doInBackground(String... params) {
-
-        SpotifyApi api = new SpotifyApi();
-        SpotifyService spotify = api.getService();
-
-        Map<String, Object> country = new HashMap<>();
-        country.put("country", "US");
-        Tracks tracks = spotify.getArtistTopTrack(params[0], country);
         ArrayList<TrackModel> results = new ArrayList<>();
 
-        for(Track track : tracks.tracks) {
-            TrackModel trackModel = new TrackModel();
-            trackModel.setId(track.id);
-            trackModel.setTitle(track.name);
-            trackModel.setAlbum(track.album.name);
+        try {
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
 
-            if(track.artists.size() > 0) {
-                trackModel.setArtist(track.artists.get(0).name);
+            Map<String, Object> country = new HashMap<>();
+            country.put("country", "US");
+            Tracks tracks = spotify.getArtistTopTrack(params[0], country);
+
+            for (Track track : tracks.tracks) {
+                TrackModel trackModel = new TrackModel();
+                trackModel.setId(track.id);
+                trackModel.setTitle(track.name);
+                trackModel.setAlbum(track.album.name);
+
+                if (track.artists.size() > 0) {
+                    trackModel.setArtist(track.artists.get(0).name);
+                }
+
+                if (track.album.images.size() > 0) {
+                    trackModel.setUrl(track.album.images.get(0).url);
+                }
+
+                results.add(trackModel);
             }
-
-            if(track.album.images.size() > 0) {
-                trackModel.setUrl(track.album.images.get(0).url);
-            }
-
-            results.add(trackModel);
+        } catch(RetrofitError error) {
+            Log.e("Error: ", error.getMessage());
+            return null;
         }
 
         return results;
@@ -73,7 +80,9 @@ public class TrackDownloader extends AsyncTask<String, Void, List<TrackModel>> {
     @Override
     protected void onPostExecute(List<TrackModel> tracks) {
 
-        if(tracks.isEmpty()) {
+        if(tracks == null) {
+            Toast.makeText(mContext, "Oh no! Something went wrong :( Make sure you have an internet connection", Toast.LENGTH_SHORT).show();
+        }else if(tracks.isEmpty()) {
             Toast.makeText(mContext, "No top tracks found for this artist. Please redefine your search", Toast.LENGTH_SHORT).show();
         } else {
             trackAdapter.setModels(tracks);
